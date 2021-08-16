466d4d229ca1a69d532e9d1df333b3ef496838d7
Matt Dean <mdean400@gmail.com>
2021-08-15 21:37:09 -0600

    Attempt to fix form

--- src/components/pages/Inquiries/Form.tsx
+++ src/components/pages/Inquiries/Form.tsx
@@ -37,6 +37,7 @@ export default function ContactForm() {
         }}
         onReset={() => setValue(defaultValue)}
         onSubmit={handleSubmit}
+        data-netlify='true'
       >
         <FormField label='Name' name='name' required />
         <FormField label='Email' name='email' required>
