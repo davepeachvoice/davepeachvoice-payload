d0f81c44aebb70664766fed42acfda2b6f526c2b
Matt Dean <mdean400@gmail.com>
2021-08-29 14:34:57 -0600

    Try to use waveform.js

---
+++ src/pages/wavesurfer-test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Waveform from '../components/WaveformPlayer/Waveform';
+
+export default function Index() {
+  return (
+    <Layout>
+      <Waveform></Waveform>
+    </Layout>
+  );
+}
