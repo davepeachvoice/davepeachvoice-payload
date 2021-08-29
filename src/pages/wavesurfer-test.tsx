40f0d04a5f7332802c6bdf1ad161aaffc9a9d50f
Matt Dean <mdean400@gmail.com>
2021-08-29 15:03:02 -0600

    wavesurfer loads audio

--- src/pages/wavesurfer-test.tsx
+++ src/pages/wavesurfer-test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import Waveform from '../components/WaveformPlayer/Waveform';
+import dynamic from 'next/dynamic';
+const Waveform = dynamic(
+  () => import('../components/WaveformPlayer/Waveform'),
+  { ssr: false }
+);
 
 export default function Index() {
   return (
