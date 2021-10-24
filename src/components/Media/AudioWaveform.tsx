e6172642d358dece4d71550a197dc217f64c4542
Matt Dean <mdean400@gmail.com>
2021-10-24 15:24:42 -0600

    Add a play/pause button to AudioWaveform
    Co-authored-by: YunSang Lee <qlqhwb@users.noreply.github.com>

--- src/components/Media/AudioWaveform.tsx
+++ src/components/Media/AudioWaveform.tsx
@@ -2,12 +2,26 @@ import { PortfolioItemInterface } from '@components/PortfolioItems/PortfolioItem
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { WaveSurfer, WaveForm } from 'wavesurfer-react';
+import { PlayFill, PauseFill } from 'grommet-icons';
+import { Button } from 'grommet';
 
 const Buttons = styled.div`
-  display: inline-block;
+  display: flex;
+  justify-content: center;
 `;
 
-const Button = styled.button``;
+const LocalButton = styled(Button)`
+  outline: none;
+  border: none;
+  background: #444;
+  border-radius: 100%;
+  width: 40px;
+  height: 40px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 interface Props {
   portfolioItem: PortfolioItemInterface;
@@ -15,18 +29,25 @@ interface Props {
 
 export default function Waveform(props: Props) {
   const [currentAudioSource, setCurrentAudioSource] = useState<string>(null);
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [bottomPosition, setBottomPosition] = useState<number>(-125);
 
   const wavesurferRef = useRef<WaveSurfer>();
 
   function handleWSMount(waveSurfer, audioSource) {
     console.log('handling mount');
+
+    if (wavesurferRef.current) {
+      wavesurferRef.current.destroy();
+    }
+
     wavesurferRef.current = waveSurfer;
     if (wavesurferRef.current && audioSource) {
       wavesurferRef.current.load(audioSource);
 
       wavesurferRef.current.on('ready', () => {
         console.log('WaveSurfer is ready');
-        wavesurferRef.current.playPause();
+        wavesurferRef.current.play();
       });
 
       wavesurferRef.current.on('region-removed', (region) => {
@@ -35,6 +56,14 @@ export default function Waveform(props: Props) {
 
       wavesurferRef.current.on('loading', (data) => {
         console.log('loading --> ', data);
+      });
+
+      wavesurferRef.current.on('pause', () => {
+        setPlaying(false);
+      });
+
+      wavesurferRef.current.on('play', () => {
+        setPlaying(true);
       });
     }
   }
@@ -58,23 +87,55 @@ export default function Waveform(props: Props) {
     setCurrentAudioSource(props.portfolioItem.media_source);
   }, [props.portfolioItem]);
 
+  // useEffect(() => {
+  //   if (playing) {
+  //     setBottomPosition(0);
+  //   } else {
+  //     setBottomPosition(-125);
+  //   }
+  // }, [playing]);
+
   return (
-    <div className='App' style={{ backgroundColor: 'white' }}>
-      {currentAudioSource}
-      <WaveSurfer
-        key={currentAudioSource}
-        onMount={(waveSurfer) => handleWSMount(waveSurfer, currentAudioSource)}
-      >
-        <WaveForm
-          barWidth={3}
-          hideScrollbar={true}
-          responsive={true}
-          height={75}
-        ></WaveForm>
-      </WaveSurfer>
-      <Buttons>
-        <Button onClick={play}>Play / Pause</Button>
-      </Buttons>
+    <div
+      className='App'
+      style={{
+        backgroundColor: 'white',
+        height: '125px',
+        position: 'fixed',
+        left: 0,
+        bottom: 0,
+        right: 0,
+      }}
+    >
+      <div style={{ marginTop: '10px' }}>
+        <WaveSurfer
+          key={currentAudioSource}
+          onMount={(waveSurfer) =>
+            handleWSMount(waveSurfer, currentAudioSource)
+          }
+        >
+          <WaveForm
+            barWidth={1}
+            hideScrollbar={true}
+            responsive={true}
+            height={75}
+            barHeight={4}
+            barGap={1}
+            normalize={true}
+            minPxPerSec={100}
+            mediaControls={true}
+          ></WaveForm>
+        </WaveSurfer>
+        <Buttons>
+          <LocalButton onClick={play}>
+            {playing ? (
+              <PauseFill style={{ marginLeft: '0' }} size='medium'></PauseFill>
+            ) : (
+              <PlayFill style={{ marginLeft: '5px' }} size='medium'></PlayFill>
+            )}
+          </LocalButton>
+        </Buttons>
+      </div>
     </div>
   );
 }
