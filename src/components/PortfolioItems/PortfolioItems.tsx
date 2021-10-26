40418bfe7db9591aba080d5362ed273d8f2ade39
Matt Dean <mdean400@gmail.com>
2021-10-25 23:26:55 -0600

    Fix issue where grid does not load right away because Grid.available is false on the initial load

--- src/components/PortfolioItems/PortfolioItems.tsx
+++ src/components/PortfolioItems/PortfolioItems.tsx
@@ -14,45 +14,25 @@ export interface PortfolioItemsProps {
  * Credit: https://github.com/grommet/grommet-site/blob/master/src/screens/Components/Section.js (modified)
  */
 export default function PortfolioItems(props: PortfolioItemsProps) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore Grid.available exists, but not on the TypeScript type https://github.com/grommet/grommet/issues/5496
-  if (Grid.available) {
-    return (
-      <Grid
-        columns={{ count: 'fill', size: ['small', 'medium'] }}
-        rows='small'
-        gap={{ row: 'medium' }}
-        justifyContent='center'
-      >
-        {props.items.map((item) => (
-          <RecordButton
-            key={item.title}
-            item={item}
-            onClick={() => {
-              console.log('clicked portfolio item action button');
-              return props.setPlayingPortfolioItem(item);
-            }}
-          ></RecordButton>
-        ))}
-      </Grid>
-    );
-  } else {
-    return (
-      <Box direction='row' wrap>
-        {props.items.map((item) => (
-          <Box basis='medium' pad='small' key={item.title}>
-            <Box basis='small'>
-              {
-                <RecordButton
-                  key={item.title}
-                  item={item}
-                  onClick={() => props.setPlayingPortfolioItem(item)}
-                ></RecordButton>
-              }
-            </Box>
-          </Box>
-        ))}
-      </Box>
-    );
-  }
+  return (
+    // TODO: consider providing a fallback if (!Grid.available)
+    // See commit ef79eded68bec2b315beb071d9ccc6691b6249ee for an incomplete starting point (formatting is off)
+    <Grid
+      columns={{ count: 'fill', size: ['small', 'medium'] }}
+      rows='small'
+      gap={{ row: 'medium' }}
+      justifyContent='center'
+    >
+      {props.items.map((item) => (
+        <RecordButton
+          key={item.title}
+          item={item}
+          onClick={() => {
+            console.log('clicked portfolio item action button');
+            return props.setPlayingPortfolioItem(item);
+          }}
+        ></RecordButton>
+      ))}
+    </Grid>
+  );
 }
