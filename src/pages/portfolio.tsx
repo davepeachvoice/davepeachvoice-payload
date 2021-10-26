e706c4f3d92a8bcc6c5c532649d368f3f7cef7ae
Matt Dean <mdean400@gmail.com>
2021-10-25 23:17:53 -0600

    Add videomodal and audiowaveform to portfolio page

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import Portfolio from '@components/Portfolio';
 import { PortfolioItemInterface } from '@components/PortfolioItems/PortfolioItemInterface';
+import VideoModal from '@components/Media/VideoModal';
+import dynamic from 'next/dynamic';
+const AudioWaveform = dynamic(() => import('@components/Media/AudioWaveform'), {
+  ssr: false,
+});
 
 export default function PortfolioPage() {
   const [
@@ -12,6 +17,11 @@ export default function PortfolioPage() {
   return (
     <Layout>
       <Portfolio setPlayingPortfolioItem={setPlayingPortfolioItem}></Portfolio>
+      <AudioWaveform portfolioItem={playingPortfolioItem}></AudioWaveform>
+      <VideoModal
+        portfolioItem={playingPortfolioItem}
+        setPortfolioItem={setPlayingPortfolioItem}
+      ></VideoModal>
     </Layout>
   );
 }
