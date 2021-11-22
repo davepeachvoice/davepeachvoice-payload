305cbe6f50b4c222b829c3f18a8abae69b2524d1
Matt Dean <mdean400@gmail.com>
2021-11-21 19:14:42 -0700

    Improve form look and split it into two sections

--- src/components/pages/Services/Form.tsx
+++ src/components/pages/Services/Form.tsx
@@ -23,7 +23,6 @@ interface FormState {
   name: string;
   email: string;
   request?: string;
-  attribution?: string;
 }
 
 const RECAPTCHA_KEY = '6Lf7CAMcAAAAACNXsN6-hnIxztE0lFyltbvAOnKu';
@@ -38,20 +37,26 @@ interface Props {
 export function ContactForm(props: Props) {
   const [value, setValue] = useState<FormState>(defaultValue);
   const recaptchaRef = useRef<Recaptcha>();
-  const [submitButtonDisabled, setButtonDisabled] = useState(true);
+  const [submitButtonEnabled, setSubmitButtonEnabled] = useState(false);
   const [fullFormVisible, setFullFormVisible] = React.useState(false);
   const [selectedService, setSelectedService] = React.useState<string>(
     undefined
   );
+  const [formStep, setFormStep] = useState(0);
+
+  function readyToNavigateToNextStep() {
+    setFormStep(formStep + 1);
+  }
+
+  function readyToNavigateToPreviousStep() {
+    setFormStep(formStep - 1);
+  }
 
   return (
-    <Box width='medium'>
+    <Box width='large'>
       <Form<FormState>
         value={value}
-        onChange={(nextValue, { touched }) => {
-          console.log('Change', nextValue, touched);
-          setValue(nextValue);
-        }}
+        onChange={setValue}
         onReset={() => setValue(defaultValue)}
         onSubmit={handleSubmit}
         data-netlify='true'
@@ -70,7 +75,7 @@ export function ContactForm(props: Props) {
             <Box
               width='100%'
               key={item.title}
-              background={selectedService === item.title ? 'white' : 'grey'}
+              background={selectedService === item.title ? '#eee' : 'grey'}
               border={{
                 color: 'black',
               }}
@@ -94,47 +99,22 @@ export function ContactForm(props: Props) {
           ))}
         </Grid>
         {fullFormVisible ? (
-          <Box>
-            <FormField label='Name' name='name' required />
-            <FormField label='Email' name='email' required>
-              <MaskedInput
-                name='email'
-                mask={[
-                  { regexp: /^[\w\-_.]+$/, placeholder: 'example' },
-                  { fixed: '@' },
-                  { regexp: /^[\w]+$/, placeholder: 'my' },
-                  { fixed: '.' },
-                  { regexp: /^[\w]+$/, placeholder: 'com' },
-                ]}
-                type='email'
-              />
-            </FormField>
-            <FormField label={props.requestFieldPrompt} name='request'>
-              <TextArea
-                name='request'
-                size='small'
-                resize='vertical'
-              ></TextArea>
-            </FormField>
-            <FormField>
-              <Recaptcha
-                ref={recaptchaRef}
-                sitekey={RECAPTCHA_KEY}
-                size='normal'
-                onChange={() => setButtonDisabled(false)}
-              />
-            </FormField>
-            <Box direction='row' justify='end' margin={{ top: 'medium' }}>
-              <Button
-                type='submit'
-                disabled={submitButtonDisabled}
-                label='Submit'
-              />
-            </Box>
+          <Box
+            margin={{
+              top: '40px',
+            }}
+          >
+            <RenderFormBody
+              formStep={formStep}
+              readyToNavigateToNextStep={readyToNavigateToNextStep}
+              readyToNavigateToPreviousStep={readyToNavigateToPreviousStep}
+              requestFieldPrompt={props.requestFieldPrompt}
+              submitButtonEnabled={submitButtonEnabled}
+              setSubmitButtonEnabled={setSubmitButtonEnabled}
+              recaptchaRef={recaptchaRef}
+            />
           </Box>
-        ) : (
-          <Box></Box>
-        )}
+        ) : null}
       </Form>
     </Box>
   );
@@ -166,4 +146,145 @@ function encode(
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&');
+}
+
+interface RenderFormBodyInput {
+  requestFieldPrompt: string;
+  setSubmitButtonEnabled: (newValue: boolean) => void;
+  submitButtonEnabled: boolean;
+  recaptchaRef: React.MutableRefObject<Recaptcha>;
+  formStep: number;
+  readyToNavigateToNextStep: () => void;
+  readyToNavigateToPreviousStep: () => void;
+}
+
+function RenderFormBody(input: RenderFormBodyInput) {
+  switch (input.formStep) {
+    case 0:
+      return <Step0 {...input}></Step0>;
+    case 1:
+      return <Step1 {...input}></Step1>;
+    default:
+      return null;
+  }
+}
+
+interface Step0Props {
+  requestFieldPrompt: string;
+  readyToNavigateToNextStep: () => void;
+}
+
+function Step0(props: Step0Props) {
+  return (
+    <Box>
+      <FormTextArea label={props.requestFieldPrompt}></FormTextArea>
+      <Box align='end'>
+        <FormButton onClick={props.readyToNavigateToNextStep} label='Next' />
+      </Box>
+    </Box>
+  );
+}
+
+interface Step1Props {
+  setSubmitButtonEnabled: (newValue: boolean) => void;
+  submitButtonEnabled: boolean;
+  recaptchaRef: React.MutableRefObject<Recaptcha>;
+  readyToNavigateToPreviousStep: () => void;
+}
+
+function Step1(props: Step1Props) {
+  return (
+    <Box>
+      <FormField label='Name' name='name' required />
+      <FormField label='Email' name='email' required>
+        <MaskedInput
+          name='email'
+          mask={[
+            { regexp: /^[\w\-_.]+$/, placeholder: 'example' },
+            { fixed: '@' },
+            { regexp: /^[\w]+$/, placeholder: 'my' },
+            { fixed: '.' },
+            { regexp: /^[\w]+$/, placeholder: 'com' },
+          ]}
+          type='email'
+        />
+      </FormField>
+      <FormField>
+        <Recaptcha
+          ref={props.recaptchaRef}
+          sitekey={RECAPTCHA_KEY}
+          size='normal'
+          onChange={() => props.setSubmitButtonEnabled(true)}
+        />
+      </FormField>
+      <Box direction='row' justify='between' margin={{ top: 'medium' }}>
+        <FormButton
+          label='Back'
+          unfilled
+          onClick={props.readyToNavigateToPreviousStep}
+        />
+        <FormButton
+          label='Submit'
+          type='submit'
+          disabled={!props.submitButtonEnabled}
+        />
+      </Box>
+    </Box>
+  );
+}
+
+interface FormButtonProps {
+  onClick?: () => void;
+  label: string;
+  disabled?: boolean;
+  // TODO: consider getting type's type from Grommet's Button
+  type?: 'button' | 'submit' | 'reset';
+  unfilled?: boolean;
+}
+
+function FormButton(props: FormButtonProps) {
+  return (
+    <Button
+      size='medium'
+      label={props.label}
+      style={{
+        width: '150px',
+        borderRadius: '5px',
+        paddingTop: '5px',
+        paddingBottom: '5px',
+        backgroundColor: props.unfilled ? '#222' : null,
+        color: props.unfilled ? 'white' : null,
+      }}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    />
+  );
+}
+
+interface FormTextAreaProps {
+  label: string;
+}
+
+function FormTextArea(props: FormTextAreaProps) {
+  return (
+    <FormField
+      label={
+        <div style={{ fontSize: '24px', marginBottom: '10px' }}>
+          {props.label}
+        </div>
+      }
+      name='request'
+    >
+      <TextArea
+        style={{
+          backgroundColor: '#eee',
+          color: '#444',
+        }}
+        color='red'
+        name='request'
+        size='small'
+        resize='vertical'
+      ></TextArea>
+    </FormField>
+  );
 }
