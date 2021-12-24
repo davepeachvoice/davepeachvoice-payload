820ede12a0babedeee49db22cba4b8fe9ff855e1
Matt Dean <mdean400@gmail.com>
2021-12-24 14:12:14 -0600

    Move homepage portfolio generation to build time

--- src/components/HomePortfolio.tsx
+++ src/components/HomePortfolio.tsx
@@ -1,41 +1,19 @@
-import React, { useEffect, useState } from 'react';
-
-import { PortfolioItemInterface } from './PortfolioItems/PortfolioItemInterface';
-import { importPortfolioItems } from 'src/import-portfolio-data';
 import PortfolioItems from '@components/PortfolioItems/PortfolioItems';
+import React from 'react';
+import { PortfolioItemInterface } from './PortfolioItems/PortfolioItemInterface';
 
 interface Props {
   setPlayingPortfolioItem: React.Dispatch<
     React.SetStateAction<PortfolioItemInterface>
   >;
+  portfolioItems: PortfolioItemInterface[];
 }
 
 export default function HomePortfolio(props: Props) {
-  const [portfolioItems, setPortfolioItems] = useState<
-    PortfolioItemInterface[]
-  >([]);
-
-  useEffect(() => {
-    async function setInitialProps() {
-      const portfolioItemsMarkdownData = await importPortfolioItems();
-
-      console.log('got portfolio items');
-      console.log(portfolioItemsMarkdownData);
-
-      const portfolioItems = portfolioItemsMarkdownData.map(
-        (portfolioItemMarkdownData) => portfolioItemMarkdownData.attributes
-      );
-
-      setPortfolioItems(portfolioItems);
-    }
-
-    setInitialProps();
-  }, []);
-
   return (
     <div>
       <PortfolioItems
-        items={portfolioItems}
+        items={props.portfolioItems}
         setPlayingPortfolioItem={props.setPlayingPortfolioItem}
       ></PortfolioItems>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,29 @@ import Layout from '@components/Layout';
 import VideoModal from '@components/Media/VideoModal';
 import HomeAudio from '@components/pages/Home/HomeAudio/HomeAudio';
 import { PortfolioItemInterface } from '@components/PortfolioItems/PortfolioItemInterface';
+import type { InferGetStaticPropsType } from 'next';
 import dynamic from 'next/dynamic';
 import React, { useEffect, useState } from 'react';
+import { importPortfolioItems } from 'src/import-portfolio-data';
 const AudioWaveform = dynamic(() => import('@components/Media/AudioWaveform'), {
   ssr: false,
 });
 
-const Index = () => {
+export async function getStaticProps() {
+  const portfolioItemsMarkdownData = await importPortfolioItems();
+
+  const portfolioItems = portfolioItemsMarkdownData.map(
+    (portfolioItemMarkdownData) => portfolioItemMarkdownData.attributes
+  );
+
+  return {
+    props: {
+      portfolioItems,
+    },
+  };
+}
+
+export function Index(props: InferGetStaticPropsType<typeof getStaticProps>) {
   const [playingPortfolioItem, setPlayingPortfolioItem] =
     useState<PortfolioItemInterface>(null);
 
@@ -27,6 +43,7 @@ const Index = () => {
       <HomeExperience></HomeExperience>
       <HomePortfolio
         setPlayingPortfolioItem={setPlayingPortfolioItem}
+        portfolioItems={props.portfolioItems}
       ></HomePortfolio>
       <AudioWaveform portfolioItem={playingPortfolioItem}></AudioWaveform>
       <VideoModal
@@ -35,6 +52,6 @@ const Index = () => {
       ></VideoModal>
     </Layout>
   );
-};
+}
 
 export default Index;
