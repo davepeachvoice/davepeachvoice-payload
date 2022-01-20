aade437c936e05306c39b5bf5086474bb93ef5c6
Matt Dean <mdean400@gmail.com>
2022-01-19 21:30:37 -0700

    Remove unnecessary .png specification on cloudinary march madness images

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -81,7 +81,7 @@ export default function Services(
                 className='next-image'
                 layout='fill'
                 objectFit='contain'
-                src='/dave-peach-web-netlify-cms/march_madness.png'
+                src='/dave-peach-web-netlify-cms/march_madness'
                 objectPosition='center top'
                 alt='Dave Peach announcing at March Madness in 2021'
                 placeholder='blur'
